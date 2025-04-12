c62cc22a1c7dd64b314c000b6563852eb4764f36
Dang Vu <nguyetning@gmail.com>
2025-04-12 11:16:48 +0700

    final asset

---
+++ [Rustica] Asset/Map/Cobble_Road_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Cobble_Road_2" tilewidth="8" tileheight="8" tilecount="60" columns="6">
+ <image source="../Cute_Fantasy/Cute_Fantasy/Tiles/Cobble_Road/Cobble_Road_2.png" width="48" height="80"/>
+</tileset>
---
+++ [Rustica] Asset/Map/Player_4x4.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Player" tilewidth="4" tileheight="4" tilecount="12800" columns="64">
+ <image source="../Old_Sprites/Old_Sprites/Player_Merged/Player_Old/Player.png" width="256" height="800"/>
+</tileset>
